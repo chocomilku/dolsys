4b621661f8404678546040566e705c4012cf0191
chocomilku- <88021521+chocomilku@users.noreply.github.com>
2023-04-28 10:52:37 +0000

    feat[server]: included auth0 errors in error responses

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
+import helmet from "helmet";
+
 import { routes } from "./routes";
 import { handleErrors } from "./middleware/handleErrors";
 
@@ -8,6 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
 
 app.use("/", routes);
 
