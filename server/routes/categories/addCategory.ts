814d926eae8f3713b8715ff511f8aefd95c1b8d1
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-04-29 16:36:11 +0000

    feat[server]: added return json from added category

--- server/routes/categories/addCategory.ts
+++ server/routes/categories/addCategory.ts
@@ -1,23 +1,36 @@
-import { Router } from "express";
+import { Response, Router } from "express";
 import { z } from "zod";
 import { addCategoryToDB } from "../../controller/addCategoryToDB";
 import { TypedRequestBody } from "../../interfaces/typedExpress";
-import { CategoriesWithoutID } from "../../../interfaces/Categories";
+import {
+	Categories,
+	CategoriesWithoutID,
+} from "../../../interfaces/Categories";
+import { db } from "../../middleware/knex/credentials";
 
 const router: Router = Router();
 
-router.post("/", (req: TypedRequestBody<CategoriesWithoutID>) => {
-	const category = req.body;
+router.post(
+	"/",
+	async (req: TypedRequestBody<CategoriesWithoutID>, res: Response) => {
+		const category = req.body;
 
-	const categorySchema = z.object({
-		name: z.string(),
-		code: z.string().max(12).optional(),
-		scope_level: z.string().max(24).optional(),
-	});
+		const categorySchema = z.object({
+			name: z.string(),
+			code: z.string().max(12).optional(),
+			scope_level: z.string().max(24).optional(),
+		});
 
-	const parsedCategory = categorySchema.parse(category);
+		const parsedCategory = categorySchema.parse(category);
 
-	addCategoryToDB(parsedCategory);
-});
+		const addedCategoryID = await addCategoryToDB(parsedCategory);
+
+		const addedCategory = await db<Categories>("categories").where({
+			id: addedCategoryID[0],
+		});
+
+		return res.status(201).json(addedCategory[0]);
+	}
+);
 
 export const addCategoryRoute: Router = router;
