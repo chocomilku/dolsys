2edbb2dc120ee53bd339fe82217b385bdbde2449
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-04-29 16:49:14 +0000

    feat[server]: implemented category listing

--- server/routes/categories/index.ts
+++ server/routes/categories/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { addCategoryRoute } from "./addCategory";
+import { listCategoriesRoute } from "./listCategories";
 
 const router: Router = Router();
+
+router.use("/", listCategoriesRoute); // POST /
+router.use("/", addCategoryRoute); // GET /
 
 export const categoriesRoutes: Router = router;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { indexRoute } from "./index.route";
 import { uploadRoutes } from "./upload";
 import { dlRoutes } from "./download";
+import { categoriesRoutes } from "./categories";
 
 const router: Router = Router();
 
 router.use("/upload", uploadRoutes); // POST /upload
+router.use("/categories", categoriesRoutes); // POST & GET /categories
 router.use("/", indexRoute); // GET / (home callback)
 router.use("/", dlRoutes); // GET /:uid
 
