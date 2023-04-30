2dbac161379becb6450590a7a73c9a5c3837c0c9
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-04-30 07:07:14 +0000

    feat[client]: wrapped Auth0Provider to other file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Auth0ProviderWithNavigate } from "./auth0ProviderWithNavigate.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-      }}
-      useRefreshTokens={true}
-      cacheLocation='localstorage'
-      >
-     <App />
-    </Auth0Provider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Auth0ProviderWithNavigate>
+				<App />
+			</Auth0ProviderWithNavigate>
+		</BrowserRouter>
+	</React.StrictMode>
+);
