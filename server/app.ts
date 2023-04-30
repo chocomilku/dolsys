5577f6be1e151b12c075f10f5e7417d9943b6181
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-04-30 06:10:51 +0000

    feat[server]: added logging using morgan and winston

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 
 import { routes } from "./routes";
 import { handleErrors } from "./middleware/errors/handleErrors";
+import morganMiddleware from "./middleware/morgan/morganLoggingMiddleware";
 
 const app: Application = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
+app.use(morganMiddleware);
 
 app.use("/", routes);
 
