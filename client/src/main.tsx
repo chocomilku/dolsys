6e808193e7436e5b5702d476c92dcd47e4c24c2b
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-04-30 13:58:35 +0000

    feat[client]: added `chakra-ui` for design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Auth0ProviderWithNavigate } from "./auth0ProviderWithNavigate.tsx";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<Auth0ProviderWithNavigate>
-				<App />
-			</Auth0ProviderWithNavigate>
+			<ChakraProvider>
+				<Auth0ProviderWithNavigate>
+					<App />
+				</Auth0ProviderWithNavigate>
+			</ChakraProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
