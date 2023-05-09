7c0ec60acca68081196b15138044bdddbefd9c20
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-05-09 05:13:55 +0000

    feat: implemented showing details of file

--- client/src/pages/index.[uid].route.tsx
+++ client/src/pages/index.[uid].route.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { axiosWrapperWithAuthToken } from "../controllers/axios/axiosWrapperWithAuthToken";
+import { useAuth0 } from "@auth0/auth0-react";
+import { FilesWithCategoriesWithoutPathAndUserID } from "../../../interfaces/FileMetadata";
 
 export const IndexUIDPage = (): JSX.Element => {
 	const { uid } = useParams();
+	const { getAccessTokenSilently } = useAuth0();
+	const [fileMetadata, setFileMetadata] =
+		useState<FilesWithCategoriesWithoutPathAndUserID>();
+
+	useEffect(() => {
+		const fetchFileMetadata = async () => {
+			try {
+				const access_token = await getAccessTokenSilently();
+				const fetchFileMetadata =
+					await axiosWrapperWithAuthToken<FilesWithCategoriesWithoutPathAndUserID>(
+						access_token,
+						{
+							url: `/files/${uid}`,
+							method: "GET",
+						}
+					);
+				if (!fetchFileMetadata.data) return;
+				setFileMetadata(fetchFileMetadata.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchFileMetadata();
+	}, [getAccessTokenSilently, uid]);
 
 	return (
 		<h1>
 			UID: <code>{uid}</code>
+			<br />
+			<code>{JSON.stringify(fileMetadata)}</code>
 		</h1>
 	);
 };
