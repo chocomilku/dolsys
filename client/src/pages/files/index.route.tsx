10b77ab1b1c57cb4bc83c86a7d2dd851cb5ac278
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-05-13 09:22:30 +0000

    feat[client]: updated file drop zone and select

--- client/src/pages/files/index.route.tsx
+++ client/src/pages/files/index.route.tsx
@@ -1,5 +1,6 @@
 import {
 	Avatar,
+	Box,
 	Button,
 	Container,
 	Flex,
@@ -9,14 +10,16 @@ import {
 	Heading,
 	Input,
 	Select,
-	Stack,
 	VStack,
+	Text,
+	Icon,
 } from "@chakra-ui/react";
-import ReactSelect from "react-select";
+import { Select as ReactSelect } from "chakra-react-select";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { type User, useAuth0 } from "@auth0/auth0-react";
 import { RxAvatar } from "react-icons/rx";
+import { BsCheckSquare, BsCloudUpload } from "react-icons/bs";
 
 export const FileIndexPage = (): JSX.Element => {
 	const [file, setFile] = useState<File>();
@@ -32,20 +35,45 @@ export const FileIndexPage = (): JSX.Element => {
 					Upload Files
 				</Heading>
 
-				<form>
+				<Flex as={"form"} direction={"column"} w={"xl"} gap="1rem">
 					<FileUploader
 						handleChange={handleChange}
 						name="file"
 						multiple={false}
 						required={true}
 						// apperently, message after upload cannot be changed yet so lets wait for https://github.com/KarimMokhtar/react-drag-drop-files/pull/118 to be merged to be able to do update the text
-					/>
-					<Grid templateColumns={"2fr 1fr"} gap="0.25rem">
+					>
+						<Flex
+							w="full"
+							borderColor={"purple.500"}
+							borderStyle={"dashed"}
+							borderWidth={"3px"}
+							p={16}
+							alignItems={"center"}
+							justifyContent={"center"}
+							flexDirection={"column"}
+							cursor={"pointer"}
+							gap="1rem">
+							<Icon
+								boxSize={"5em"}
+								as={!file ? BsCloudUpload : BsCheckSquare}
+							/>
+							<Text fontSize={"xl"}>
+								{!file
+									? "Drop your file or click here"
+									: "Uploaded Successfully"}
+							</Text>
+						</Flex>
+					</FileUploader>
+
+					<Grid templateColumns={"2fr 1fr"} gap="0.5rem">
 						<GridItem as={FormControl}>
 							<Input
 								type="text"
 								placeholder={file?.name ?? "File Name"}
 								isReadOnly
+								isTruncated
+								colorScheme="purple"
 							/>
 						</GridItem>
 						<GridItem as={FormControl}>
@@ -53,33 +81,49 @@ export const FileIndexPage = (): JSX.Element => {
 								isReadOnly
 								placeholder={user?.name ?? "User"}
 								icon={<AvatarIcon user={user} />}
+								colorScheme="purple"
 							/>
 						</GridItem>
 					</Grid>
 
 					<FormControl>
-						<Input type="text" placeholder="Name" />
+						<Input
+							type="text"
+							placeholder="Name"
+							required
+							colorScheme="purple"
+						/>
 					</FormControl>
 
 					<Flex gap="0.25rem">
-						<ReactSelect
-							options={[
-								{ value: "chocolate", label: "Chocolate" },
-								{ value: "strawberry", label: "Strawberry" },
-								{ value: "vanilla", label: "Vanilla" },
-							]}
-						/>
+						<Box w={"full"}>
+							<ReactSelect
+								required
+								options={[
+									{ value: "chocolate", label: "Chocolate" },
+									{ value: "strawberry", label: "Strawberry" },
+									{ value: "vanilla", label: "Vanilla" },
+								]}
+								colorScheme="purple"
+							/>
+						</Box>
 
 						<FormControl maxW={"100px"}>
-							<Input type="text" placeholder="Phase" />
+							<Input type="text" placeholder="Phase" colorScheme="purple" />
 						</FormControl>
 
 						<FormControl maxW={"100px"}>
-							<Input type="text" placeholder="Unit" />
+							<Input type="text" placeholder="Unit" colorScheme="purple" />
 						</FormControl>
 					</Flex>
-				</form>
-				<Button>Upload</Button>
+				</Flex>
+				<Button
+					variant={"solid"}
+					colorScheme="purple"
+					size={"lg"}
+					w={{ base: "sm", md: "lg" }}>
+					Upload
+				</Button>
 			</VStack>
 		</>
 	);
