72f70cb77af4b9ca84bf3dfec16b43117ac187de
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-05-13 04:04:05 +0000

    feat[client]: added hero

--- client/src/pages/index.route.tsx
+++ client/src/pages/index.route.tsx
@@ -1,3 +1,30 @@
+import { Button, Container, Flex, Heading, Spacer } from "@chakra-ui/react";
+import HeroBg from "../../assets/hero-bg.svg";
+
 export const IndexPage = (): JSX.Element => {
-	return <h1>Hello World!</h1>;
+	return (
+		<>
+			<Flex
+				alignItems="center"
+				justifyContent="center"
+				flexDirection="column"
+				p={20}
+				style={{
+					background: `url(${HeroBg})`,
+					width: "100%",
+					height: "calc(35vh + 12rem)",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+				}}>
+				<Heading as={"h2"} size="4xl" textColor={"purple.900"}>
+					some text here 🚀
+				</Heading>
+				<Spacer />
+				<Button size="lg" w={{ base: "xs", md: "lg" }}>
+					Log In
+				</Button>
+			</Flex>
+			<Container maxW="container.xl">a</Container>
+		</>
+	);
 };
