a2538e6f5ec1c2ab6edd53f77bea78828c1bdfd4
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-05-14 11:17:00 +0000

    feat![server]: implemented pagination

--- server/controller/getFileMetadata.ts
+++ server/controller/getFileMetadata.ts
@@ -9,6 +9,24 @@ export const getFileMetadata = async (
 		.first();
 };
 
-export const getFilesMetadata = async () => {
-	return await db<FilesWithCategories>("files_with_categories");
+interface GetFilesMetadataParams {
+	offset?: number;
+	limit?: number;
+	orderBy?: "asc" | "desc";
+}
+
+export const getFilesMetadata = async (params?: GetFilesMetadataParams) => {
+	const OFFSET = 0;
+	const LIMIT = 10;
+	const ORDER_BY: GetFilesMetadataParams["orderBy"] = "asc";
+
+	const [offset, limit, orderBy] = [
+		params?.offset,
+		params?.limit,
+		params?.orderBy,
+	];
+	return await db<FilesWithCategories>("files_with_categories")
+		.offset(offset ?? OFFSET)
+		.limit(limit ?? LIMIT)
+		.orderBy("id", orderBy ?? ORDER_BY);
 };
--- server/routes/files/listFilesMetadata.ts
+++ server/routes/files/listFilesMetadata.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getFilesMetadata } from "../../controller/getFileMetadata";
 import { authMiddleware } from "../../middleware/jwt-bearer/authOptions";
 import { checkRequiredPermissions } from "../../middleware/auth0/checkPermissions";
+import { BadRequestError } from "../../middleware/errors/errors";
 
 const router: Router = Router();
 
@@ -9,16 +10,40 @@ router.get(
 	"/",
 	authMiddleware,
 	checkRequiredPermissions(["view:files"]),
-	async (req, res) => {
-		const files = await getFilesMetadata();
+	async (req, res, next) => {
+		try {
+			const { page = "1", limit = "10" } = req.query;
+			const [parsedPage, parsedLimit] = [
+				parseInt(page.toString()),
+				parseInt(limit.toString()),
+			];
 
-		const filesWithoutPathAndUser = files.map((file) => {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const { path, user_id, ...fileWithoutPathAndUser } = file;
-			return fileWithoutPathAndUser;
-		});
+			if (isNaN(parsedPage)) throw new BadRequestError("Invalid page number");
+			if (isNaN(parsedLimit)) throw new BadRequestError("Invalid limit number");
 
-		return res.status(200).json(filesWithoutPathAndUser);
+			if (parsedPage < 1) throw new BadRequestError("Invalid page number");
+			if (parsedLimit < 1) throw new BadRequestError("Invalid limit number");
+
+			const offset = (parsedPage - 1) * parsedLimit;
+
+			if (isNaN(offset)) throw new BadRequestError("Invalid offset");
+			if (offset < 0) throw new BadRequestError("Invalid offset");
+
+			const files = await getFilesMetadata({
+				limit: parseInt(limit.toString()),
+				offset,
+			});
+
+			const filesWithoutPathAndUser = files.map((file) => {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				const { path, user_id, ...fileWithoutPathAndUser } = file;
+				return fileWithoutPathAndUser;
+			});
+
+			return res.status(200).json(filesWithoutPathAndUser);
+		} catch (error) {
+			next(error);
+		}
 	}
 );
 
