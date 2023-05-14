fe66701d0bb9b95ac7d2d3744840e24457f23612
chocomilku <88021521+chocomilku@users.noreply.github.com>
2023-05-14 02:41:16 +0000

    feat[client]: implemented basic file listing

--- client/src/pages/files/index.route.tsx
+++ client/src/pages/files/index.route.tsx
@@ -1,4 +1,18 @@
-import { Code, Container, Heading, VStack } from "@chakra-ui/react";
+import {
+	Box,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Code,
+	Container,
+	Heading,
+	IconButton,
+	Stack,
+	VStack,
+	Text,
+} from "@chakra-ui/react";
+import { FiDelete } from "react-icons/fi";
 import { FilesWithCategoriesWithoutPathAndUserID } from "../../../../interfaces/FileMetadata";
 import { useEffect, useState } from "react";
 import { axiosWrapperWithAuthToken } from "../../controllers/axios/axiosWrapperWithAuthToken";
@@ -32,7 +46,31 @@ export const FilesIndexPage = (): JSX.Element => {
 				<Heading textAlign="center" pb={8}>
 					Files
 				</Heading>
-				<Code>{JSON.stringify(filesList)}</Code>
+				<Stack direction="column" spacing="4">
+					{filesList.map((file, index) => {
+						return (
+							<>
+								<Box
+									key={index}
+									display="flex"
+									alignItems="center"
+									justifyContent="space-between"
+									p={2}
+									borderWidth={1}
+									borderRadius="md">
+									<Text>{file.originalname}</Text>
+									<IconButton
+										icon={<FiDelete />}
+										variant="ghost"
+										colorScheme="red"
+										onClick={() => console.log("e")}
+										aria-label="Delete file"
+									/>
+								</Box>
+							</>
+						);
+					})}
+				</Stack>
 			</VStack>
 		</>
 	);
